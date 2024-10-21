c1bc7c0101cbd890e431d3302c2261887dc684fa
Anand987 <anandsharma110397@gmail.com>
2024-10-21 05:38:00 +0530

    updated [auth] : modified signup to add cookie after user creation
    and added create auth cookie logic to handle cookie creation based on id

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -1,11 +1,25 @@
 "use server";
 
+import { createAuthSession } from "@/lib/auth";
 import { hashUserPassword } from "@/lib/hash";
 import { createUser } from "@/lib/users";
 import { redirect } from "next/navigation";
 
 const PASS_LEN = 8;
 
+/**
+ * Handles the user signup process by validating the provided email and password,
+ * hashing the password, and creating a new user account. If validation fails, 
+ * an error object is returned. If the signup is successfull, the user is redirected
+ * to the training page.
+ *
+ * @param {Object} prevState - The previous state of the application (optional, can be used for tracking changes).
+ * @param {FormData} formData - The form data containing user inputs, email and password.
+ * @returns {Promise<Object>} A promise that resolves to an object containing errors (if any) 
+ * or redirects the user upon successfull signup.
+ * 
+ * @throws {Error} Throws an error if an unexpected issue occurs during the signup process.
+ */
 export async function signup(prevState, formData) {
   const email = formData.get("email");
   const password = formData.get("password");
@@ -28,7 +42,9 @@ export async function signup(prevState, formData) {
   const hashedPassword = hashUserPassword(password);
 
   try {
-    createUser(email, hashedPassword);
+    const id = createUser(email, hashedPassword);
+    await createAuthSession(id);
+    redirect("/training");
   } catch (error) {
     if (error.code === "SQLITE_CONSTRAINT_UNIQUE") {
       return {
@@ -40,6 +56,4 @@ export async function signup(prevState, formData) {
     }
     throw error;
   }
-
-  redirect("/training");
 }
